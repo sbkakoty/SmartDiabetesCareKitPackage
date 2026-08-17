5838e6654696a6f5a59244b075b6b199b664000a
sbkakoty <sbkakoty@gmail.com>
2026-08-17 14:48:08 +0530

    Update SmartDiabetesCareKit XCFramework to 1.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SmartDiabetesCareKit",
-            url: "https://github.com/sbkakoty/SmartDiabetesCareKit/releases/download/1.0.0/SmartDiabetesCareKit.xcframework.zip",
+            url: "https://github.com/sbkakoty/SmartDiabetesCareKit/releases/download/1.1.0/SmartDiabetesCareKit.xcframework.zip",
             checksum: "21b57d8c35086ec1508de9e5406bd81079e072775980b01133e7c7b00c513a66"
         )
     ]
